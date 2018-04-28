1c9af06d11671a26c8526b10aae649c8ffb1e059
oriechinedu <oriechinedu>
2018-04-28 20:37:24 +0100

    correct misspelling

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,7 +19,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 });
 
 $factory->define(App\Reservation::class, function (Faker\Generator $faker) {
-    $rand_str = ['Not paid', 'Piad'];
+    $rand_str = ['Not paid', 'Paid'];
     $index = rand(0,1);
     $comment = rand(0,5);
     $rand_int = [0,1];
